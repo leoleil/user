ff355e0f13499c7984880332c7ee210936da878f
切克闹 <zhangzy249@nenu.edu.cn>
2019-04-09 01:33:33 +0800

    表A update delete 表B update

--- src/main/java/com/onps/service/serviceimpl/schedule/ProjectScheduleFormAServiceImpl.java
+++ src/main/java/com/onps/service/serviceimpl/schedule/ProjectScheduleFormAServiceImpl.java
@@ -1,8 +1,10 @@
 package com.onps.service.serviceimpl.schedule;
 
 import com.onps.dao.ProjectMapper;
+import com.onps.dao.SubprojectMapper;
 import com.onps.model.Project;
 import com.onps.model.ProjectExample;
+import com.onps.model.SubprojectExample;
 import com.onps.model.vo.ProjectScheduleVO;
 import com.onps.service.ProjectScheduleFormAService;
 import com.onps.service.UserSessionService;
@@ -12,11 +14,14 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
 @Service
 public class ProjectScheduleFormAServiceImpl implements ProjectScheduleFormAService {
-
     @Autowired
     private ProjectMapper projectMapper;
+    @Autowired
+    private SubprojectMapper subprojectMapper;
     /**
      * 更新表A内容
      *  通过            andProjectnameEqualTo(projectScheduleVO.getProjectName())
@@ -38,16 +43,18 @@ public class ProjectScheduleFormAServiceImpl implements ProjectScheduleFormAServ
         //通过5个level和projectname唯一一条主项目
         criteria1.andProjectnameEqualTo(projectScheduleVO.getProjectName())
                 .andLevel1EqualTo(projectScheduleVO.getLevel1())
-                .andLevel2EqualTo(projectScheduleVO.getLevel2())
+               // .andLevel2EqualTo(projectScheduleVO.getLevel2())
                 .andLevel3EqualTo(projectScheduleVO.getLevel3())
                 .andLevel4EqualTo(projectScheduleVO.getLevel4())
                 .andLevel5EqualTo(projectScheduleVO.getLevel5());
+        if (projectScheduleVO.getLevel2()!=null){
+            criteria1.andLevel2EqualTo(projectScheduleVO.getLevel2());
+        }
+
         //把传入的PO类每个字段进行判空，如果不为空，则写入project对象
-        /*if(projectScheduleVO.getProjectName() != null){
-        project.setProjectname(projectScheduleVO.getProjectName());
-        }*/
         if(projectScheduleVO.getDocumentName()!=null){
-        project.setDocumentname(projectScheduleVO.getDocumentName());}
+        project.setDocumentname(projectScheduleVO.getDocumentName());
+        }
         if(projectScheduleVO.getDocumentNumber()!=null){
             project.setDocumentnumber(projectScheduleVO.getDocumentNumber());
         }
@@ -66,26 +73,12 @@ public class ProjectScheduleFormAServiceImpl implements ProjectScheduleFormAServ
         if(projectScheduleVO.getIsIsimportent()!=null){
             project.setIsimportent(projectScheduleVO.getIsIsimportent());
         }
-        /*if(projectScheduleVO.getLevel1()!=null){
-            project.setLevel1(project.getLevel1());
-        }
-        if(projectScheduleVO.getLevel2()!=null){
-            project.setLevel2(project.getLevel2());
-        }
-        if(projectScheduleVO.getLevel3()!=null){
-            project.setLevel3(project.getLevel3());
-        }
-        if(projectScheduleVO.getLevel4()!=null){
-            project.setLevel4(project.getLevel4());
-        }
-        if(projectScheduleVO.getLevel5()!=null){
-            project.setLevel5(project.getLevel5());
-        }*/
         if(projectScheduleVO.getSubmitCompany()!=null){
             project.setSubmitcompany(project.getSubmitcompany());
         }
 
         projectMapper.updateByExampleSelective(project,projectExp);
+
 
     }
 
@@ -97,6 +90,28 @@ public class ProjectScheduleFormAServiceImpl implements ProjectScheduleFormAServ
      */
     @Override
     public void deleteASchedule(ProjectScheduleVO projectScheduleVO) throws Exception {
+        Project project=new Project();
+        ProjectExample projectExp1=new ProjectExample();//用来select projct
+        ProjectExample.Criteria criteria2=projectExp1.createCriteria();
+        SubprojectExample subprojectExp1=new SubprojectExample();//用来delete subproject
+        SubprojectExample.Criteria subcriteria=subprojectExp1.createCriteria();
+        //通过5个level和projectname唯一一条主项目
+        criteria2.andProjectnameEqualTo(projectScheduleVO.getProjectName())
+                .andLevel1EqualTo(projectScheduleVO.getLevel1())
+             //   .andLevel2EqualTo(projectScheduleVO.getLevel2())
+                .andLevel3EqualTo(projectScheduleVO.getLevel3())
+                .andLevel4EqualTo(projectScheduleVO.getLevel4())
+                .andLevel5EqualTo(projectScheduleVO.getLevel5());
+        if (projectScheduleVO.getLevel2()!=null){
+            criteria2.andLevel2EqualTo(projectScheduleVO.getLevel2());
+        }
 
+        List<Project> projectList= projectMapper.selectByExample(projectExp1);
+        String id=projectList.get(0).getId();//得到主项目id
+        subcriteria.andProjectidEqualTo(id);
+        subprojectMapper.deleteByExample(subprojectExp1);//删除projectId=主项目id的所有子项目
+
+
+        projectMapper.deleteByPrimaryKey(id);
     }
 }
--- src/main/java/com/onps/service/serviceimpl/schedule/ProjectScheduleFormBServiceImpl.java
+++ src/main/java/com/onps/service/serviceimpl/schedule/ProjectScheduleFormBServiceImpl.java
@@ -1,9 +1,22 @@
 package com.onps.service.serviceimpl.schedule;
 
+import com.onps.dao.ProjectMapper;
+import com.onps.dao.SubprojectMapper;
+import com.onps.model.Project;
+import com.onps.model.ProjectExample;
+import com.onps.model.Subproject;
+import com.onps.model.SubprojectExample;
 import com.onps.model.vo.ProjectScheduleVO;
 import com.onps.service.ProjectScheduleFormBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProjectScheduleFormBServiceImpl implements ProjectScheduleFormBService {
+    @Autowired
+    private ProjectMapper projectMapper;
+    @Autowired
+    private SubprojectMapper subprojectMapper;
     /**
      * 更新表B内容
      *
@@ -12,7 +25,105 @@ public class ProjectScheduleFormBServiceImpl implements ProjectScheduleFormBServ
      */
     @Override
     public void updateBSchedule(ProjectScheduleVO projectScheduleVO) throws Exception {
+        Subproject subproject=new Subproject();
+        //从表B中找出对应的project
+             //projectExample用来匹配五个level,和主项目名称，通过这几个字段得到对应的projectID
+        ProjectExample projectExample=new ProjectExample();
+        ProjectExample.Criteria criteria1=projectExample.createCriteria();
+        criteria1. andProjectnameEqualTo(projectScheduleVO.getProjectName())
+                .andLevel1EqualTo(projectScheduleVO.getLevel1())
+                .andLevel3EqualTo(projectScheduleVO.getLevel3())
+                .andLevel4EqualTo(projectScheduleVO.getLevel4())
+                .andLevel5EqualTo(projectScheduleVO.getLevel5());
+        //对Level2需要进行判空，如果不为空，再进行筛选
+                if(projectScheduleVO.getLevel2() !=null){
+                    criteria1.andLevel2EqualTo(projectScheduleVO.getLevel2());
+                }
+        String projectId=projectMapper.selectByExample(projectExample).get(0).getId();
+            //subprojectExample 通过projectID和subprojectName匹配子项目
+        SubprojectExample subprojectExample=new SubprojectExample();
+        SubprojectExample.Criteria criteria2=subprojectExample.createCriteria();
+        criteria2.andSubnameEqualTo(projectScheduleVO.getSubName())
+                 .andProjectidEqualTo(projectId);
 
+        //对每个字段进行判空，把非空的字段进行update
+        if(projectScheduleVO.getSubName() !=null){
+        subproject.setSubname(projectScheduleVO.getSubName());}
+        if(projectScheduleVO.getStartTime() !=null){
+       subproject.setStarttime(projectScheduleVO.getStartTime());}
+        if(projectScheduleVO.getSubmitFileName() !=null){
+       subproject.setSubmitfilename(projectScheduleVO.getSubmitFileName());}
+        if(projectScheduleVO.getSubmitDepartment() !=null){
+       subproject.setSubmitdepartment(projectScheduleVO.getSubmitDepartment());}
+        if(projectScheduleVO.getSubmitNumber() !=null){
+       subproject.setSubmitnumber(projectScheduleVO.getSubmitNumber());}
+        if(projectScheduleVO.getSubmitDate() !=null){
+       subproject.setSubmitdate(projectScheduleVO.getSubmitDate());}
+        if(projectScheduleVO.getApprovalName() !=null){
+       subproject.setApprovalname(projectScheduleVO.getApprovalName());}
+        if(projectScheduleVO.getApprovalDepartment() !=null){
+       subproject.setApprovaldepartment(projectScheduleVO.getApprovalDepartment());}
+        if(projectScheduleVO.getApprovalNumber() !=null){
+       subproject.setApprovalnumber(projectScheduleVO.getApprovalNumber());}
+        if(projectScheduleVO.getApprovalDate() !=null){
+       subproject.setApprovaldate(projectScheduleVO.getApprovalDate());}
+        if(projectScheduleVO.getSubmitFileNameTa() !=null){
+       subproject.setSubmitfilenameTa(projectScheduleVO.getSubmitFileNameTa());}
+        if(projectScheduleVO.getSubmitDepartmentTa() !=null){
+       subproject.setSubmitdepartmentTa(projectScheduleVO.getSubmitDepartmentTa());}
+        if(projectScheduleVO.getSubmitNumberTa() !=null){
+       subproject.setSubmitnumberTa(projectScheduleVO.getSubmitNumberTa());}
+        if(projectScheduleVO.getSubmitDateTa() !=null){
+       subproject.setSubmitdateTa(projectScheduleVO.getSubmitDateTa());}
+        if(projectScheduleVO.getApprovalNameTa() !=null){
+       subproject.setApprovalnameTa(projectScheduleVO.getApprovalNameTa());}
+        if(projectScheduleVO.getApprovalDepartmentTa() !=null){
+       subproject.setApprovaldepartmentTa(projectScheduleVO.getApprovalDepartmentTa());}
+        if(projectScheduleVO.getApprovalNumberTa() !=null){
+       subproject.setApprovalnumberTa(projectScheduleVO.getApprovalNumberTa());}
+        if(projectScheduleVO.getApprovalDateTa() !=null){
+       subproject.setApprovaldateTa(projectScheduleVO.getApprovalDateTa());}
+        if(projectScheduleVO.getReplyDate() !=null){
+       subproject.setReplydate(projectScheduleVO.getReplyDate());}
+        if(projectScheduleVO.getApprovalProgress() != null){
+       subproject.setApprovalprogress(projectScheduleVO.getApprovalProgress());}
+        if(projectScheduleVO.getConstructionContent() != null){
+       subproject.setConstructioncontent(projectScheduleVO.getConstructionContent());}
+        if(projectScheduleVO.getProjectNumber() != null){
+       subproject.setProjectnumber(projectScheduleVO.getProjectNumber());}
+        if(projectScheduleVO.getConstructionPlace() != null){
+       subproject.setConstructionplace(projectScheduleVO.getConstructionPlace());}
+        if(projectScheduleVO.getConstructionCompany() !=null){
+       subproject.setConstructioncompany(projectScheduleVO.getConstructionCompany());}
+        if(projectScheduleVO.getEndTime() != null){
+       subproject.setEndtime(projectScheduleVO.getEndTime());}
+        if(projectScheduleVO.getReleaseInvestment() !=null){
+       subproject.setReleaseinvestment(projectScheduleVO.getReleaseInvestment());}
+        if(projectScheduleVO.getConstructionPhase() !=null){
+       subproject.setConstructionphase(projectScheduleVO.getConstructionPhase());}
+        if(projectScheduleVO.getFirstDesign() !=null){
+       subproject.setFirstdesign(projectScheduleVO.getFirstDesign());}
+        if(projectScheduleVO.getBidding() !=null){
+       subproject.setBidding(projectScheduleVO.getBidding());}
+        if(projectScheduleVO.getConstructionDesign() !=null){
+       subproject.setConstructiondesign(projectScheduleVO.getConstructionDesign());}
+        if(projectScheduleVO.getStartTimeCon() !=null){
+       subproject.setStarttimeCon(projectScheduleVO.getStartTimeCon());}
+        if(projectScheduleVO.getProjectProgress()!=null){
+       subproject.setProjectprogress(projectScheduleVO.getProjectProgress());}
+        if(projectScheduleVO.getCompletedInvestment() !=null){
+       subproject.setCompletedinvestment(projectScheduleVO.getCompletedInvestment());}
+        if(projectScheduleVO.getFormedAbility()!=null){
+       subproject.setFormedability(projectScheduleVO.getFormedAbility());}
+        if(projectScheduleVO.getDesignedAbility() !=null){
+       subproject.setDesignedability(projectScheduleVO.getDesignedAbility());}
+        if(projectScheduleVO.getRemarks()!=null){
+       subproject.setRemarks(projectScheduleVO.getRemarks());}
+        if(projectScheduleVO.getProjectId() !=null){
+       subproject.setProjectid(projectScheduleVO.getProjectId());}
+        if(projectScheduleVO.getDepartment() !=null){
+       subproject.setDepartment(projectScheduleVO.getDepartment());}
+        subprojectMapper.updateByExampleSelective(subproject,subprojectExample);
     }
 
     /**
