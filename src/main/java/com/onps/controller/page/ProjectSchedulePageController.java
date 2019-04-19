c2b745db074424429cdd3c8a81784e2840a1c6c8
leoleil <leoleil@163.com>
2019-04-19 22:45:23 +0800

    版本一联通,与甲方前端进行适配

--- src/main/java/com/onps/controller/page/ProjectSchedulePageController.java
+++ src/main/java/com/onps/controller/page/ProjectSchedulePageController.java
@@ -1,27 +1,29 @@
 package com.onps.controller.page;
 
+import com.onps.utils.resultUtils.BaseResult;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/schedule")
 public class ProjectSchedulePageController {
     @RequestMapping(value = "/index")
-    @RequiresRoles("user1")
+    @RequiresRoles("user")
     public String toScheduleIndexPage(){
         String returnUrl = "schedule/index";
         return returnUrl;
     }
     @RequestMapping(value = "/view")
-    @RequiresRoles("user1")
+    @RequiresRoles("user")
     public String toScheduleViewPage(){
         String returnUrl = "schedule/view";
         return returnUrl;
     }
     @RequestMapping(value = "/form")
-    @RequiresRoles("user1")
+    @RequiresRoles("user")
     public String toScheduleFormPage(){
         String returnUrl = "schedule/form";
         return returnUrl;
